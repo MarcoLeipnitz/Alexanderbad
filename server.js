1772fdbff4d411c9c867bcfe9ae68c7897fbb7e8
MarcoLeipnitz <marco.leipnitz@gmx.net>
2024-04-30 19:57:41 +0200

    Added website compatibility

---
+++ server.js
@@ -0,0 +1,8 @@
+var http = require('http');
+var fs = require('fs');+
+
+http.createServer(function (req, res) {
+    res.writeHead(200);
+    res.write(fs.readFileSync("index.html"));
+    res.end();
+}).listen(8080);
