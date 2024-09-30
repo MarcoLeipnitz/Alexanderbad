6be42a022404f8d5fc06cb4d1208858b059830d7
Marco <marco.leipnitz@gmx.net>
2024-09-30 11:09:14 +0200

    Added Until 2025 note

--- server.js
+++ server.js
@@ -3,6 +3,13 @@ var http = require('http');
 var fs = require('fs');
 
 http.createServer(function (req, res) {
+    if(req.url == "/images/Nachbereitung_100Jahre.webp"){
+        res.writeHead(200);
+        res.write(fs.readFileSync("./images/Nachbereitung_100Jahre.webp"));
+        res.end();
+        return;
+    }
+
     res.writeHead(200);
     res.write(fs.readFileSync("index.html"));
     res.end();
